6b941c63d707cc4868ce1556f2cff51a91aa14d6
u88872625 <u88872625@gmail.com>
2024-01-15 20:21:00 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const express = require("express");
+const socketIo = require("socket.io");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Runnging on:${PORT}`);
+});
