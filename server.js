8cb53a60e580360e6532c0716228c026e99a85f6
u88872625 <u88872625@gmail.com>
2024-03-28 17:21:29 +0800

    feat: upadate cors origin

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://u88872625.github.io"],
     methods: ["GET", "POST"],
     credentials: true,
   },
